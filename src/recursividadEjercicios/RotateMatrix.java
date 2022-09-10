c782c487c577fa3bf7fbe00bb1b6e300e1135237
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2022-09-10 16:12:24 -0500

    Commit provisional

--- src/recursividadEjercicios/RotateMatrix.java
+++ src/recursividadEjercicios/RotateMatrix.java
@@ -17,36 +17,75 @@ public class RotateMatrix {
 
         rotateMatrixClockWiseOnce(m, n, 0, 0);
 
-        Utils.print(n);
+        //Utils.print(n);
+
+        int[][] x = new int[m[0].length][m.length];
+
+        rotateMatrixAntiClockWiseOnce(m, x, 0, 0);
+        Utils.print(x);
 
 
     }
 
 
     public static void rotateMatrixClockWiseOnce(int[][] m, int[][] n, int i, int j) {
-
-
-        if (j == m[0].length) {
-            j = 0;
-            i++;
-            if (i == m.length) return;
+        if (i == m.length) {
+            i = 0;
+            j++;
+            if (j == m[0].length) return;
         }
-        n[i][m.length - 1 - j] = m[i][j];
-        rotateMatrixClockWiseOnce(m, n, i, ++j);
+        n[j][m.length - 1 - i] = m[i][j];
+        i++;
+        rotateMatrixClockWiseOnce(m, n, i, j);
     }
 
-    public void rotateMatrixClockWise(int[][] m, int n) {
+
+    public static void rotateMatrixAntiClockWiseOnce(int[][] m, int[][] n, int i, int j) {
+        if (i == m.length) {
+            i = 0;
+            j++;
+            if (j == m[0].length) return;
+        }
+        n[j][i] = m[i][j];
+        i++;
+        rotateMatrixAntiClockWiseOnce(m, n, i, j);
+    }
+
+
+    //  public static int[][] rotateClockWise (int[][]m, int[][]n, int times){
+//
+    //      if(times>0) {
+    //          int[][] rotated = new int[m[0].length][m.length];
+    //          rotateMatrixAntiClockWiseOnce(m, n, 0, 0);
+    //          times--;
+    //      }
+    //  }
+
+
+    /**
+     * Método que retorna una matriz girada una n cantidad de veces en el sentido dispuesto en los parametros.
+     *
+     * @param matrix     Matriz a girar
+     * @param direcction True para sentido horario, False para antihorario.
+     * @param times      Cantidad de veces a girar la matriz
+     */
+    public int[][] rotateMatrixClockWise(int[][] matrix, boolean direcction, int times) {
 
         int[][] newMatrix;
 
-        if (n % 2 == 0) {
-            newMatrix = new int[m[0].length][m.length];
+        if (times % 4 == 0) return matrix;
 
-            rotateMatrixClockWise(m, --n);
+        if (times % 2 == 0 && direcction && times > 0) {
+            newMatrix = new int[matrix[0].length][matrix.length];
+
+            rotateMatrixClockWiseOnce(matrix, newMatrix, 0, 0);
 
         } else {
-            newMatrix = new int[m.length][m[0].length];
+            newMatrix = new int[matrix.length][matrix[0].length];
+
         }
 
+
+        return newMatrix;
     }
 }
